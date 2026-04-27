e7e6fae8ddd30f55295916ef302cd425ce3f9c6e
dukuzejean09 <dukuzejean09@gmail.com>
2026-04-27 14:44:51 +0200

    ok

---
+++ backend/alembic/versions/012_merge_case_and_auto_grouping_heads.py
@@ -0,0 +1,22 @@
+"""Merge case history and auto-grouping migration heads
+
+Revision ID: 012_merge_case_and_auto_grouping_heads
+Revises: 011, 010_auto_grouping_case_metadata
+Create Date: 2026-04-27
+"""
+
+from typing import Sequence, Union
+
+
+revision: str = "012_merge_case_and_auto_grouping_heads"
+down_revision: Union[str, Sequence[str], None] = ("011", "010_auto_grouping_case_metadata")
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
